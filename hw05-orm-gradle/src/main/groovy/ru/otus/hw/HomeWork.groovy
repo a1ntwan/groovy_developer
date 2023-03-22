39c84aed9e2d9606e3f5980b9496ca28021d0340
Anton_Kulagin <ai.kulagin@jet.su>
2023-03-22 03:28:03 +0300

    Добавил сборку с помощью Jib

--- hw05-orm-gradle/src/main/groovy/ru/otus/hw/HomeWork.groovy
+++ hw05-orm-gradle/src/main/groovy/ru/otus/hw/HomeWork.groovy
@@ -17,75 +17,81 @@ import javax.sql.DataSource
 
 import static org.slf4j.LoggerFactory.getLogger
 
+class HomeWork {
+    static void main(String[] args) {
 
-Properties properties = new Properties()
-File propertiesFile = new File('application.properties')
-propertiesFile.withInputStream {
-    properties.load(it)
-}
+        Properties properties = new Properties()
+        File propertiesFile = new File('application.properties')
+        propertiesFile.withInputStream {
+            properties.load(it)
+        }
 
-final URL = "${properties.getProperty('URL')}"
-final USER = "${properties.getProperty('USER')}"
-final PASSWORD = "${properties.getProperty('PASSWORD')}"
+        final URL = "${properties.getProperty('URL')}"
+        final USER = "${properties.getProperty('USER')}"
+        final PASSWORD = "${properties.getProperty('PASSWORD')}"
 
-final Logger log = getLogger(HomeWork.class)
+        final Logger log = getLogger(HomeWork.class)
 
 
-// Общая часть
-def dataSource = new DriverManagerDataSource(URL, USER, PASSWORD)
-flywayMigrations(dataSource)
-def transactionRunner = new TransactionRunnerJdbc(dataSource)
-def dbExecutor = new DbExecutorImpl()
+        // Общая часть
+        def dataSource = new DriverManagerDataSource(URL, USER, PASSWORD)
+        flywayMigrations(dataSource)
+        def transactionRunner = new TransactionRunnerJdbc(dataSource)
+        def dbExecutor = new DbExecutorImpl()
 
-// Работа с клиентом
-def entityClassMetaDataClient = new EntityClassMetaDataImpl<>(Client.class)
-def entitySQLMetaDataClient = new EntitySQLMetaDataImpl<>(entityClassMetaDataClient)
-def dataTemplateClient = new DataTemplateJdbc(dbExecutor, entitySQLMetaDataClient, entityClassMetaDataClient) //реализация DataTemplate, универсальная
+        // Работа с клиентом
+        def entityClassMetaDataClient = new EntityClassMetaDataImpl<>(Client.class)
+        def entitySQLMetaDataClient = new EntitySQLMetaDataImpl<>(entityClassMetaDataClient)
+        def dataTemplateClient = new DataTemplateJdbc(dbExecutor, entitySQLMetaDataClient, entityClassMetaDataClient)
+        //реализация DataTemplate, универсальная
 
-// Код дальше должен остаться
-def dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient)
-def clientAfterSave = dbServiceClient.saveClient(new Client("dbServiceFirst"))
-log.info(">>>after save::{}", clientAfterSave)
-def clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"))
-def clientSecondSelected = dbServiceClient.getClient(clientSecond.getId())
-        .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()))
-log.info("clientSecondSelected:{}", clientSecondSelected)
-log.info(">>>All clients::{}", dbServiceClient.findAll())
-def clientForUpdate = clientSecondSelected
-clientForUpdate.setName("New name for client")
-def clientAfterUpdate = dbServiceClient.saveClient(clientForUpdate as Client)
+        // Код дальше должен остаться
+        def dbServiceClient = new DbServiceClientImpl(transactionRunner, dataTemplateClient)
+        def clientAfterSave = dbServiceClient.saveClient(new Client("dbServiceFirst"))
+        log.info(">>>after save::{}", clientAfterSave)
+        def clientSecond = dbServiceClient.saveClient(new Client("dbServiceSecond"))
+        def clientSecondSelected = dbServiceClient.getClient(clientSecond.getId())
+                .orElseThrow(() -> new RuntimeException("Client not found, id:" + clientSecond.getId()))
+        log.info("clientSecondSelected:{}", clientSecondSelected)
+        log.info(">>>All clients::{}", dbServiceClient.findAll())
+        def clientForUpdate = clientSecondSelected
+        clientForUpdate.setName("New name for client")
+        def clientAfterUpdate = dbServiceClient.saveClient(clientForUpdate as Client)
 
-log.info(">>>Last client after update::{}", clientAfterUpdate)
-// Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
+        log.info(">>>Last client after update::{}", clientAfterUpdate)
+        // Сделайте тоже самое с классом Manager (для него надо сделать свою таблицу)
 
-def entityClassMetaDataManager = new EntityClassMetaDataImpl<>(Manager.class)
-def entitySQLMetaDataManager = new EntitySQLMetaDataImpl<>(entityClassMetaDataManager)
-def dataTemplateManager = new DataTemplateJdbc(dbExecutor, entitySQLMetaDataManager, entityClassMetaDataManager)
+        def entityClassMetaDataManager = new EntityClassMetaDataImpl<>(Manager.class)
+        def entitySQLMetaDataManager = new EntitySQLMetaDataImpl<>(entityClassMetaDataManager)
+        def dataTemplateManager = new DataTemplateJdbc(dbExecutor, entitySQLMetaDataManager, entityClassMetaDataManager)
 
-def dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager)
-def managerAfterSave = dbServiceManager.saveManager(new Manager("ManagerFirst"))
-log.info(">>>after save::{}", managerAfterSave)
+        def dbServiceManager = new DbServiceManagerImpl(transactionRunner, dataTemplateManager)
+        def managerAfterSave = dbServiceManager.saveManager(new Manager("ManagerFirst"))
+        log.info(">>>after save::{}", managerAfterSave)
 
-def managerSecond = dbServiceManager.saveManager(new Manager("ManagerSecond"))
-def managerSecondSelected = dbServiceManager.getManager(managerSecond.getNo())
-        .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()))
-log.info("managerSecondSelected:{}", managerSecondSelected)
-log.info(">>>All managers::{}", dbServiceManager.findAll())
+        def managerSecond = dbServiceManager.saveManager(new Manager("ManagerSecond"))
+        def managerSecondSelected = dbServiceManager.getManager(managerSecond.getNo())
+                .orElseThrow(() -> new RuntimeException("Manager not found, id:" + managerSecond.getNo()))
+        log.info("managerSecondSelected:{}", managerSecondSelected)
+        log.info(">>>All managers::{}", dbServiceManager.findAll())
 
-def managerForUpdate = managerSecondSelected
-managerForUpdate.setLabel("New Label for manager")
-managerForUpdate.setParam1("New param for manager")
-def managerAfterUpdate = dbServiceManager.saveManager(managerForUpdate)
-log.info(">>>Last manager after update::{}", managerAfterUpdate)
+        def managerForUpdate = managerSecondSelected
+        managerForUpdate.setLabel("New Label for manager")
+        managerForUpdate.setParam1("New param for manager")
+        def managerAfterUpdate = dbServiceManager.saveManager(managerForUpdate)
+        log.info(">>>Last manager after update::{}", managerAfterUpdate)
 
-private static void flywayMigrations(DataSource dataSource) {
-    def logger = getLogger(HomeWork.class)
-    logger.info("db migration started...")
-    def flyway = Flyway.configure()
-            .dataSource(dataSource)
-            .locations("classpath:/db/migration")
-            .load()
-    flyway.migrate()
-    logger.info("db migration finished.")
-    logger.info("***")
+    }
+
+    private static void flywayMigrations(DataSource dataSource) {
+        def logger = getLogger(HomeWork.class)
+        logger.info("db migration started...")
+        def flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:/db/migration")
+                .load()
+        flyway.migrate()
+        logger.info("db migration finished.")
+        logger.info("***")
+    }
 }
